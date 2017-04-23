f592e72245f7c348a86e9b1d41738c6ce8d82d5d
Melanie <mlpatt23@gmail.com>
2017-04-23 13:17:55 -0700

    Added JS files and updated functions.php to include JS files

--- functions.php
+++ functions.php
@@ -11,6 +11,13 @@
 		
 		wp_register_style('style', get_template_directory_uri() . '/style.css', array('normalize'), '1.0');
 		
+		wp_register_script('jquery', 'https://code.jquery.com/jquery-3.2.1.min.js"
+  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+  crossorigin="anonymous', array(), '3.2.1');
+		
+		wp_register_script('jqueryrotate', get_template_directory_uri() . '/js/jqueryrotate.js', array('jquery'), '2.3', true);
+		
+		wp_register_script('script', get_template_directory_uri() . '/js/script.js', array('jquery'), '1.0', true);
 		
 		
 		// Enqueue the styles and JS
@@ -19,6 +26,9 @@
 		wp_enqueue_style('normalize');
 		wp_enqueue_style('style');
 		
+		wp_enqueue_script('jquery');
+		wp_enqueue_script('jqueryrotate');
+		wp_enqueue_script('script');
 		
 		
 	}
