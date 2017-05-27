02a507402d1c480d9ca5f26826a56c451e4a40c3
Melanie <mlpatt23@gmail.com>
2017-05-26 17:38:20 -0700

    Created blog archive page

--- index.php
+++ index.php
@@ -1,14 +1,39 @@
 <?php get_header(); ?>
 
-	<?php if ( have_posts() ) : ?>
-
-		<?php while(have_posts()) : the_post(); ?>
-			<div class="heading"><h1><?php the_title(); ?></h1></div>
-		
-			<p><?php the_content(); ?></p>
-		
-		<?php endwhile; ?>
 	
-	<?php endif; ?>
+	<div class="wrapper">
+		<main>
+			<div class="heading">
+				<h1><?php echo get_the_title(84); ?></h1>
+			</div> <!-- heading -->
+			
+			<h2>Recent Posts</h2>
+			
+			<?php if ( have_posts() ) :
+				while(have_posts()) : the_post(); ?>
+			
+					<article class="blog-post">
+						<h3>
+							<a href="<?php the_permalink(); ?>">
+								<?php the_title(); ?>
+							</a>
+						</h3>
+						
+						<div class="blog-info">
+							<!-- Include Date and Category Info -->
+						</div> <!--blog-info -->
+				
+						<?php the_excerpt(); ?>
+						
+						<a href="<?php the_permalink(); ?>">Read More...</a>
+					</article> <!-- blog-post -->
+			
+				<?php endwhile; ?>
+			<?php endif; ?>
+			
+		</main>
+	</div> <!-- wrapper -->
 		
+		
+
 <?php get_footer(); ?>
