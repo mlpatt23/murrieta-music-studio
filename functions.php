2d5ce7993580c1052dd801fecc9449bca27eff64
Melanie <mlpatt23@gmail.com>
2017-07-29 18:06:20 -0700

    Enqueued ginger files

--- functions.php
+++ functions.php
@@ -9,7 +9,9 @@
 		
 		wp_register_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '2.0');
 		
-		wp_register_style('style', get_template_directory_uri() . '/style.css', array('normalize'), '1.0');
+		wp_register_style('ginger-grid', get_template_directory_uri() . '/css/ginger.min.css', array(), '2.1.1');
+		
+		wp_register_style('style', get_template_directory_uri() . '/style.css', array('normalize', 'ginger-grid'), '1.0');
 		
 		wp_register_script('jquery', 'https://code.jquery.com/jquery-3.2.1.min.js"
   integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
@@ -17,20 +19,23 @@
 		
 		wp_register_script('jqueryrotate', get_template_directory_uri() . '/js/jqueryrotate.js', array('jquery'), '2.3', true);
 		
-		wp_register_script('script', get_template_directory_uri() . '/js/script.js', array('jquery'), '1.0', true);
+		wp_register_script('ginger-grid', get_template_directory_uri() . '/js/ginger.min.js', array(), '2.1.1');
+		
+		wp_register_script('script', get_template_directory_uri() . '/js/script.js', array('jquery', 'ginger-grid'), '1.0', true);
 		
 		
 		// Enqueue the styles and JS
 		wp_enqueue_style('googlefonts');
 		wp_enqueue_style('fontawesome');
 		wp_enqueue_style('normalize');
+		wp_enqueue_style('ginger-grid');
 		wp_enqueue_style('style');
 		
 		wp_enqueue_script('jquery');
 		wp_enqueue_script('jqueryrotate');
+		wp_enqueue_script('ginger-grid');
 		wp_enqueue_script('script');
-		
-		
+	
 	}
 	
 	add_action('wp_enqueue_scripts', 'murrietamusicstudio_styles');
